76ec9d1e31dcc00d7e29aa8c184ebc020c1657e1
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-01-13 15:12:31 +0800

    fisrt commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>CdD Contact Tracing</title>
+    <title>Colegio de Dagupan Investiture Registration</title>
 
   
 
