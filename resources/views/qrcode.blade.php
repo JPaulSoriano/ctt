efcd054cff3953cb879fb253d26c20a3b3503c64
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-06-02 13:46:55 +0800

    UPDATE

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center my-5">
         <div class="col-md-4">
             <p class="text-center text-primary font-weight-bold">
-                PRESENT THIS QR TO THE BEFORE ENTERING THE UNIVERSIDAD DE DAGUPAN.
+                PRESENT THIS QR TO STUDENTS AFFAIRS OFFICE.
             </p>
         </div>
     </div>
@@ -17,12 +17,14 @@
                         <div class="col-lg-12 h4 font-weight-bold text-center my-3 text-uppercase">
                             {{$registration->full_name}}
                         </div>
+                        <p>{{$registration->reg_ref}}</p>
                         <div class="col-lg-12 my-3 text-center">
                             <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(250)
                              ->merge(public_path('images/logo.png'), 0.3, true)->errorCorrection('H')
                              ->color(0, 28, 64)
                              ->margin(3)
-	                         ->generate(url('/registrations')."/".$registration->id)); !!} ">
+	                        //  ->generate(url('/registrations')."/".$registration->id)); !!} ">
+                             ->generate(url('/status?status=').$registration->reg_ref)); !!} ">
                          </div>
                     </div>
                 </div>
