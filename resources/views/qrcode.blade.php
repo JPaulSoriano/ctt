d84676605ac32dc72d59f7604c48337d8ffbef22
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-06-02 15:41:05 +0800

    UPDATE

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -13,12 +13,12 @@
         <div class="col-md-4">
             <div class="card shadow-sm border-0">
                 <div class="card-body">
-                    <div class="row d-flex-justify-content-center">
-                        <div class="col-lg-12 h4 font-weight-bold text-center my-3 text-uppercase">
+                    <div class="row d-flex-justify-content-center text-center">
+                        <div class="col-lg-12 h4 font-weight-bold my-3 text-uppercase">
                             {{$registration->full_name}}
                         </div>
-                        <p class="text-center">{{$registration->reg_ref}}</p>
-                        <div class="col-lg-12 my-3 text-center">
+                        <p>{{$registration->reg_ref}}</p>
+                        <div class="col-lg-12 my-3">
                             <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(250)
                              ->merge(public_path('images/logo.png'), 0.3, true)->errorCorrection('H')
                              ->color(0, 28, 64)
